2f2b4cffeffffcaf8332f10e0ea7bf76ee79b9e1
bandarichandana-2926 <bandarichandana96@gmail.com>
2023-02-01 12:10:49 +0530

    ok

---
+++ object.java
@@ -0,0 +1,9 @@
+public class object {
+    void H1(){
+        System.out.println("object creation");
+    }
+    public static void main(String args[]){
+        object obj = new object();
+        obj.H1();
+    }
+}
